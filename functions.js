bf5bd80b296e5164a4acedcdeb508bc6a01747f9
Ross <rosshamey@gmail.com>
2022-11-22 11:34:39 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -7,3 +7,19 @@ function wordWithinArray(array, word) {
         return false;
     }
 };
+
+function firstAndLast(array) {
+    let length = array.length;
+    let last = (array.length) - 1;
+    let first = 0;
+
+
+    if (length % 2 === 0) {
+        let sum = array[first] + array[last];
+        return sum;
+    } else if (length % 2 !== 0) {
+        let difference = array[first] - array[last];
+        return difference;
+    }
+
+}
