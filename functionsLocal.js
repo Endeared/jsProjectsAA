2bf3cb6d395680e1a57d061fe9058300a4314749
Ross <rosshamey@gmail.com>
2022-11-23 13:52:37 -0500

    upd

--- functionsLocal.js
+++ functionsLocal.js
@@ -25,3 +25,27 @@ let factorsOf = function(num) {
 
     return factorsArray;
 }
+
+
+// takes in a positive integer max, returns an array of all positive integers less than max that fit fizzbuzz (lol)
+let fizzBuzz = function(max) {
+    if (typeof max !== 'number') {
+        return "Not a number!";
+    } else if (max < 0) {
+        return "Not a positive integer!"
+    }
+
+    let fizzBuzzArray = [];
+    let i = 0;
+
+    for (i = 3; i < max; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push(i);
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push(i);
+        }
+    }
+
+    return fizzBuzzArray;
+}
