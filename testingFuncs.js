2e79db5d705fb76ccaea2cbc2c4bd56ff9eb0eea
Ross <rosshamey@gmail.com>
2022-11-26 17:35:16 -0500

    Update testingFuncs.js

--- testingFuncs.js
+++ testingFuncs.js
@@ -6,3 +6,40 @@ for (let i = 0; i < dogs.length; i++) {
         console.log(dog1, dog2);
     }
 }
+
+
+testArr.forEach(function(value, i) {
+    console.log(value + ", index of: " + i);
+})
+
+
+
+let newValues = values.map(function(value, i) {
+    return("VALUE: " + value.toUpperCase());
+})
+console.log(newValues);
+
+
+
+let yValues = values.filter(function(value) {
+    return value.length > 7;
+})
+console.log(yValues);
+
+
+
+let sum = nums.reduce(function(accum, el) {
+    return accum * el;
+})
+console.log(sum);
+
+
+
+let largeCheck = nums.reduce(function(accum, el) {
+    if (el > accum) {
+        return el;
+    } else {
+        return accum;
+    }
+})
+console.log(largeCheck);
